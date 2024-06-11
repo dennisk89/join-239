0bbece68c87802b658ca1dd495e55319e2378d5e
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-06-11 12:08:20 +0200

    added simple js functions

--- js/contacts.js
+++ js/contacts.js
@@ -2,3 +2,8 @@ function showOverlayEditContact() {
     let overlayEditContact = document.getElementById('overlayEditContact');
     overlayEditContact.classList.remove('hide');
 }
+
+function showOverlayEditDelete() {
+    let overlayEditDelete = document.getElementById('overlayEditDelete');
+    overlayEditDelete.classList.remove('hide');
+}
