4ff8873126e11a68a8f4d3b63bce410ac9b60ec3
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-06-12 10:47:49 +0200

    contacts responsive design

--- js/contacts.js
+++ js/contacts.js
@@ -1,9 +1,14 @@
-function showOverlayEditContact() {
+function handleOverlayEditContact() {
     let overlayEditContact = document.getElementById('overlayEditContact');
-    overlayEditContact.classList.remove('hide');
+    overlayEditContact.classList.toggle('hide');
 }
 
 function showOverlayEditDelete() {
     let overlayEditDelete = document.getElementById('overlayEditDelete');
     overlayEditDelete.classList.remove('hide');
+}
+
+function handleOverlayAddContact() {
+    let overlayAddContact = document.getElementById('overlayAddContact');
+    overlayAddContact.classList.toggle('hide');
 }
