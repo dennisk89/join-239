dd5a2d63b94e9f7190edf8c980faf8e2802db381
jjb6000 <jonas.benkwitz@gmail.com>
2024-06-14 11:29:23 +0200

    board shorter add functions

--- js/board.js
+++ js/board.js
@@ -1,12 +1,11 @@
 
 // ANCHOR load Tasks
-function callAddCardsWithFilteredArrays() {
+function initBoard() {
     addCardsToBoards('toDoColumn', todoTasks, 'To do');
     addCardsToBoards('inProgressColumn', progressTasks, 'In progress');
     addCardsToBoards('feedbackColumn', feedbackTasks, 'Await feedback');
     addCardsToBoards('doneColumn', doneTasks, 'Done');
-    addContactLabels();
-    addSubTaskProgressToCards();
+    addInfosToCards();
 }
 
 
@@ -17,23 +16,29 @@ function addCardsToBoards(columnID, filterArray, stringForEmptyColumn) {
         element.innerHTML = boardPlaceholderHTML(stringForEmptyColumn);
     } else {
         for (let i = 0; i < filterArray.length; i++) {
-            element.innerHTML += taskCardHTML(filterArray[i].id, filterArray[i].type, filterArray[i].title, filterArray[i].description, prioIcons[filterArray[i].prio]); 
+            element.innerHTML += taskCardHTML(filterArray[i].id, filterArray[i].type, filterArray[i].title, filterArray[i].description, prioIcons[filterArray[i].prio]);
         }
     }
 }
 
 
-function addContactLabels() {
+function addInfosToCards() {
     for (let i = 0; i < taskArray.length; i++) {
-        let max = getLabelMaximum(taskArray[i].assigned)
-        for (let j = 0; j < max; j++) {
-            let initials = getInitials(taskArray[i].assigned[j]);
-            let container = document.getElementById(taskArray[i].id).children[3].children[0];
-            if (j == 0) {
-                container.innerHTML += addAssignHTML(initials); 
-            } else {
-                container.innerHTML += addAssignWithOverlapHTML(initials, j);
-            }
+        addContactLabels(i);
+        addSubTaskProgressToCards(i);
+    }
+}
+
+
+function addContactLabels(i) {
+    let max = getLabelMaximum(taskArray[i].assigned)
+    for (let j = 0; j < max; j++) {
+        let initials = getInitials(taskArray[i].assigned[j]);
+        let container = document.getElementById(taskArray[i].id).children[3].children[0];
+        if (j == 0) {
+            container.innerHTML += addAssignHTML(initials);
+        } else {
+            container.innerHTML += addAssignWithOverlapHTML(initials, j);
         }
     }
 }
@@ -48,14 +53,12 @@ function getLabelMaximum(contacts) {
 }
 
 
-function addSubTaskProgressToCards() {
-    for (let i = 0; i < taskArray.length; i++) {
-        let pbar = document.getElementById(taskArray[i].id).children[2].children[0].children[0];
-        let label = document.getElementById(taskArray[i].id).children[2].children[1];
-        let done = taskArray[i].subTaskStatus.filter(s => s == true);
-        label.innerHTML = /*html*/`${done.length}/${taskArray[i].subTask.length} Subtasks`;
-        pbar.style.width = ((done.length / taskArray[i].subTask.length) * 100) +'%'; 
-    }
+function addSubTaskProgressToCards(i) {
+    let pbar = document.getElementById(taskArray[i].id).children[2].children[0].children[0];
+    let label = document.getElementById(taskArray[i].id).children[2].children[1];
+    let done = taskArray[i].subTaskStatus.filter(s => s == true);
+    label.innerHTML = /*html*/`${done.length}/${taskArray[i].subTask.length} Subtasks`;
+    pbar.style.width = ((done.length / taskArray[i].subTask.length) * 100) + '%';
 }
 
 
@@ -69,9 +72,9 @@ function getInitials(name) {
 
 function saveTasks() {
     let newTask = new Task(
-        taskArray.length + 1, 
-        'assets\img\board-card-label-us.svg', 
-        'Kochwelt html', 
+        taskArray.length + 1,
+        'assets\img\board-card-label-us.svg',
+        'Kochwelt html',
         'Write html code for Kochwelt',
         '2024-7-1',
         ['Homer Simpson', 'Gordon Shumway'],
