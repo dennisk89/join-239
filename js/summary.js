dee9d765b80b9af48ebc44702e9f54614065a1a4
Anita Gasteiner <anita.gasteiner@posteo.de>
2024-06-29 17:40:33 +0200

    show right greeting on summary page

--- js/summary.js
+++ js/summary.js
@@ -8,7 +8,6 @@ async function initSummary() {
     showTasksAwaitingFeedbackCount();
     findEarliestDate();
     getUserNameByLoggedInEmail(loggedInEmail);
-    showGreeting();
 }
 
 function showTodosCount() {
@@ -59,4 +58,70 @@ function findEarliestDate() {
 function getUserNameByLoggedInEmail(loggedInEmail) {
     const user = usersArray.find(user => user.email === loggedInEmail);
     return user ? user.name : null;
+}
+
+
+/**
+ * This function is used to select the right greeting depending on if a user is signed in or not.
+ */
+function showGreeting(yesOrNo) {
+    if (yesOrNo === 'yes') {
+        showGreetingLoggedInUser();
+    } else {
+        showGreetingGuestUser();
+    }
+}
+
+/**
+ * This function is used to show the right greeting on the summary page for logged in users.
+ */
+function showGreetingLoggedInUser() {    
+    let greetingCenter = document.getElementById('greetingCenter');
+    let greetingRight = document.getElementById('greetingRight');
+    greetingCenter.innerHTML = '';
+    greetingRight.innerHTML = '';
+    greetingCenter.innerHTML = chooseGreeting() + ',';
+    greetingRight.innerHTML = chooseGreeting() + ',';
+}
+
+/**
+ * This function is used to show the right greeting on the summary page for guest users.
+ */
+function showGreetingGuestUser() {    
+    let greetingCenter = document.getElementById('greetingCenter');
+    let greetingRight = document.getElementById('greetingRight');
+    greetingCenter.innerHTML = '';
+    greetingRight.innerHTML = '';
+    greetingCenter.innerHTML = chooseGreeting() + '!';
+    greetingRight.innerHTML = chooseGreeting() + '!';
+}
+
+/**
+ * This function is used to check the current hour and choose the greeting based on it.
+ */
+function chooseGreeting() {
+    let d = new Date();
+    let h = d.getHours();
+    if (h >= 4 && h < 10) {
+        return 'Good morning';
+    } else if (h < 4 && h >= 10 && h < 13) {
+        return  'Hello';
+    } else if (h >= 13 && h < 17) {
+        return 'Good afternoon';
+    } else if (h >= 17) {
+        return 'Good evening';
+    }
+}
+
+/**
+ * This function is used to show the name of the currently logged in user to greet him*her on the summary page.
+ * @param {string} name This is the name of the currently logged in user.
+ */
+function showUserName(name) {
+    let userNameCenter = document.getElementById('userNameCenter');
+    userNameCenter.innerHTML = '';
+    userNameCenter.innerHTML = name;
+    let userNameRight = document.getElementById('userNameRight');
+    userNameRight.innerHTML = '';
+    userNameRight.innerHTML = name;
 }
