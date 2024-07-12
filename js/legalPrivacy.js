7c24178c0c34c590c938908ed83cb7c3be0bab84
dennisk89 <dennisk8989@gmail.com>
2024-07-12 10:59:50 +0200

    merge

--- js/legalPrivacy.js
+++ js/legalPrivacy.js
@@ -1,14 +1,43 @@
 /**
  * Removes flags from localStorage when the backArrows element is clicked.
  */
-document.getElementById('backArrows').addEventListener('click', function() {
-    localStorage.removeItem('openedByLegalNotice');
-    localStorage.removeItem('openedByPrivacyPolicy');
-});
 
-document.getElementById('backArrows2').addEventListener('click', function() {
+
+function removeLocalStorage() {
     localStorage.removeItem('openedByLegalNotice');
     localStorage.removeItem('openedByPrivacyPolicy');
+}
+
+/**
+ * Hides the footer links in the mobile footer.
+ */
+function hideFooterLinks() {
+    const footer = document.querySelector('.mobile-footer');
+    if (footer) {
+        const footerLinks = footer.querySelectorAll('.footer-menu');
+        footerLinks.forEach(link => {
+            link.style.display = 'none';
+        });
+    }
+}
+
+
+/**
+ * Event listener for DOMContentLoaded to check localStorage flags and hide footer links if necessary.
+ */
+document.addEventListener("DOMContentLoaded", function() {
+    const openedByLegalNotice = localStorage.getItem('openedByLegalNotice');
+    const openedByPrivacyPolicy = localStorage.getItem('openedByPrivacyPolicy');
+
+    if (openedByLegalNotice === 'true') {
+        hideFooterLinks();
+        localStorage.removeItem('openedByLegalNotice');
+    }
+
+    if (openedByPrivacyPolicy === 'true') {
+        hideFooterLinks();
+        localStorage.removeItem('openedByPrivacyPolicy');
+    }
 });
 
 
